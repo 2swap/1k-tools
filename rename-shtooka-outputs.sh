a85da2c5057ab07d959b366db8b22ae55a329b0c
2swap <mchontz10@gmail.com>
2022-02-21 14:37:52 -0800

    Add sed rule for slashes in filenames

--- rename-shtooka-outputs.sh
+++ rename-shtooka-outputs.sh
@@ -14,7 +14,7 @@ fi
 mkdir "$1/renamed"
 for f in $(ls $1 | grep "\.flac");
 do
-        TITLE=$(mediainfo "$1/$f" | grep 'Track name ' | awk -F': ' '{print $2}' | sed "s/?//")
+        TITLE=$(mediainfo "$1/$f" | grep 'Track name ' | awk -F': ' '{print $2}' | sed "s/?//" | sed "s/\//slash/g")
 	echo "$f\t->\t$TITLE"
         cp "$1/$f" "$1/renamed/$2_$TITLE.flac"
 done
